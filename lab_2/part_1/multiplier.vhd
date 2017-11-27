a575c2d8b50c3de46130f05488f23e52c0542228
kanaljen <sbn14007@student.mdh.se>
2017-11-27 21:14:40 +0100

    Added map
    La till en bild över hur vi pratade om att döpa komponenterna, och signaler

--- lab_2/part_1/multiplier.vhd
+++ lab_2/part_1/multiplier.vhd
@@ -12,6 +12,23 @@ END multiplier;
 
 architecture dataflow of multiplier is
 
+    component full_adder is
+        Port ( A :  in STD_LOGIC;       -- First input
+               B :  in STD_LOGIC;       -- Second input
+               CIN : in STD_LOGIC;      -- Carry in
+               COUT : out STD_LOGIC;    -- Carry out
+               S : out STD_LOGIC);      -- Adder out
+    end component;
+
+	component AdderBlock is
+	    port ( adderA : in STD_LOGIC;   -- first input for the full adder
+	           adderB : in STD_LOGIC;   -- second imput for the full adder
+	           andA : in STD_LOGIC;     -- first input for the and gate
+	           andB : in STD_LOGIC;     -- second imput for the and gate
+	           sum : out STD_LOGIC;     -- the sum output
+	           carry : out STD_LOGIC);  -- the carry output
+	end component;
+
 begin
 
 end architecture ; -- dataflow
