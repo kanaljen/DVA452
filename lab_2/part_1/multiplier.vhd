c83be540fd00d0b2436b5ed8f23d430bc445278b
kanaljen <sbn14007@student.mdh.se>
2017-11-27 14:57:19 +0100

    addad multiplier.vhd
    La till en dummy fil som vi skriver topkomponenten i.

---
+++ lab_2/part_1/multiplier.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY multiplier  IS 
+	PORT( a,  b:  IN  STD_LOGIC_VECTOR  (3  DOWNTO  0);       
+		  prod:  OUT  STD_LOGIC_VECTOR  (7  DOWNTO  0)); 
+END multiplier; 
+
+architecture dataflow of multiplier is
+
+begin
+
+end architecture ; -- dataflow
