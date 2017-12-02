8ffff1b5da7bd633a0349a51922255cfbc078fa8
kanaljen <kanaljen@users.noreply.github.com>
2017-12-02 16:33:40 +0100

    Finite state machine complete

--- lab_3/part_2/fsm.vhd
+++ lab_3/part_2/fsm.vhd
@@ -8,32 +8,37 @@ end fsm;
 
 architecture process_model of fsm is
 	
-	type state is (S2,S1,S0);
-	signal pstate : state := S0; 
-	signal nstate : state;
-	signal clk : std_logic := '0';
+	type state is (S0,S1,S2,S3);
+	signal pstate : state := S3; 
+	signal nstate : state := S0; 
+	signal clk : std_logic := '1';
 
 	begin -- process_model arch
 
-	synced_process :process(clk)
+	statesetter :process(clk)
 		
 		begin
-		
-			if (pstate = S0) then
 
-				pstate <= S1;
-				out1 <= '1';
-				out2 <= '0';
+		pstate <= nstate; -- set new present state
 
-			elsif (pstate = S1) then
+			case pstate is -- set next state
+   				when S0 => nstate <= S1;
+   				when S1 => nstate <= S2;
+   				when S2 => nstate <= S3;
+   				when S3 => nstate <= S0;
+   				when others => report "state failure" severity failure;
+			end case;
 
-				pstate <= S0;
-				out1 <= '0';
-				out2 <= '1';
-				
-			end if ;
+	end process; -- statesetter
 
-	end process; -- synced_process
+	-- set out-signals
+	with pstate select
+		out1 <= '1' when S0,
+				'0' when others;
+
+	out2 <= '1' when (pstate = S0) else
+			'0' when (pstate = S1 AND clk = '1') else
+			'1'	when (pstate = S1 AND clk = '0') else '0';
 
 	-- clock running at a generic time cycle
 	clk <= not clk after cycle/2; 
