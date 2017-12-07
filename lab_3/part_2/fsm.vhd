fbf770319e78a47ca416fda08989cec66cbf2735
Kanaljen <kanaljen@users.noreply.github.com>
2017-12-07 09:44:04 +0100

    Added 2nd version
    La till Rasmus version av lab_3/part_2

--- lab_3/part_2/fsm.vhd
+++ lab_3/part_2/fsm.vhd
@@ -1,45 +1,63 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity fsm is
-
-	port(clk : in std_logic;
-		 out1, out2 : out std_logic);
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           out1 : out STD_LOGIC;
+           out2 : out STD_LOGIC);
 end fsm;
 
-architecture process_model of fsm is
-	
-	type state is (S0,S1,S2,S3);
-	signal pstate : state := S0; 
-	signal nstate : state; 
+architecture Behavioral of fsm is
+TYPE state is (state0, state1, state2, state3, state4, state5, state6, state7); 
+Signal pr_state, nx_state: state; 
+begin
+PROCESS (rst,clk)
+Begin
+    IF(rst='1') THEN	-- reset signal, restart at state zero.
+        pr_state <= state0;
+    ELSIF(clk'EVENT AND clk='1') THEN -- switches state when clock is on its rising edge.
+        pr_state <= nx_state;    
+    ELSIF(clk'EVENT AND clk='0')THEN -- switches state when clock is on its falling edge.
+        pr_state <= nx_state; 
+    END IF;
+END PROCESS;
 
-	begin -- process_model arch
-
-	statesetter :process(clk)
-		
-		begin
-
-		if rising_edge(clk) then
-			pstate <= nstate; -- set new present state
-		end if;
-
-			case pstate is -- set next state
-   				when S0 => nstate <= S1;
-   				when S1 => nstate <= S2;
-   				when S2 => nstate <= S3;
-   				when S3 => nstate <= S0;
-   				when others => report "state failure" severity failure;
-			end case;
-
-	end process; -- statesetter
-
-	-- set out-signals
-	with pstate select
-		out1 <= '1' when S0,
-				'0' when others;
-
-	out2 <= '1' when (pstate = S0) else
-			'0' when (pstate = S1 AND clk = '1') else
-			'1'	when (pstate = S1 AND clk = '0') else '0';
-
-end architecture ; -- process_model arch
+PROCESS (pr_state) -- activated when the state changes
+Begin
+    CASE pr_state IS				-- Entering present state
+        WHEN state0 =>
+            out1 <= '1';			-- Setting the desired output
+            out2 <= '1';			-- Setting the desired output
+            nx_state <= state1;     -- Setting next state to state2   
+        WHEN state1 =>
+            out1 <= '1';
+            out2 <= '1';
+            nx_state <= state2;
+        WHEN state2 =>
+            out1 <= '0';
+            out2 <= '0';
+            nx_state <= state3;  
+        WHEN state3 =>
+            out1 <= '0';
+            out2 <= '1';
+            nx_state <= state4;
+        WHEN state4 =>
+            out1 <= '0';
+            out2 <= '0';
+            nx_state <= state5;        
+        WHEN state5 =>
+            out1 <= '0';
+            out2 <= '0';
+            nx_state <= state6;
+        WHEN state6 =>
+            out1 <= '0';
+            out2 <= '0';
+            nx_state <= state7;  
+        WHEN state7 =>
+            out1 <= '0';
+            out2 <= '0';
+            nx_state <= state0;
+    END CASE;    
+END PROCESS;
+end Behavioral;
