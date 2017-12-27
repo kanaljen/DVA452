d1edcf31ae96fd99585e202887f7e0c1b5c9f630
Kanaljen <kanaljen@users.noreply.github.com>
2017-12-27 14:14:11 +0100

    Training script
    Scriptet för att få fram vikterna

---
+++ project/matLAB/train_net.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%Training a MLP NN
+load('ex3data1.mat');
+x1=X';% Input Data Set
+y1=dummyvar(y');%Output Data Set
+y1 = y1';
+net = patternnet([10,10], 'traingdx');
+net.inputs{1}.processFcns = {'mapminmax'};
+%net.inputs.processFcns = {'mapminmax'};
+net = train(net, x1, y1);
+
+net
