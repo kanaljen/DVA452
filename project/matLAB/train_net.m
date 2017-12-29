ad75f6b543f50f4223e86bc1db34a9180cdb2b1c
kanaljen <kanaljen@users.noreply.github.com>
2017-12-29 15:37:42 +0100

    Matlab Magic

--- project/matLAB/train_net.m
+++ project/matLAB/train_net.m
@@ -3,9 +3,10 @@ load('ex3data1.mat');
 x1=X';% Input Data Set
 y1=dummyvar(y');%Output Data Set
 y1 = y1';
-net = patternnet([10,10], 'traingdx');
+net = patternnet([10], 'traingdx');
 net.inputs{1}.processFcns = {'mapminmax'};
 %net.inputs.processFcns = {'mapminmax'};
 net = train(net, x1, y1);
 
-net
+W_inputs = net.IW{1};
+W_layer1 = net.LW{2};
