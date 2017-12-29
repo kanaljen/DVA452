0d70637a21f9fdf77a83e1b4063eeefe9827ab2c
kanaljen <kanaljen@users.noreply.github.com>
2017-12-29 15:22:13 +0100

    New LUT generator

---
+++ project/python/convert_func.py
@@ -0,0 +1,60 @@
+
+def binaryToDecimal(binary):
+    """"Function converts binary fraction to decimal fraction"""
+
+    dec = 0
+
+    # Only sign bit
+    if len(binary) < 1:
+        return 0
+
+    # For positive numbers
+    if(binary[0] == '0'):
+        for i in range(1,len(binary)):
+            if(binary[i] == '1'):
+                dec += (1/(2**i))
+        return dec
+
+    # For negative numbers
+    elif(binary[0] == '1'):
+        for i in range(1,len(binary)):
+            if(binary[i] == '0'):
+                dec += (1/(2**i))
+        return dec*-1
+
+
+def decToBinary(dec,bits):
+    """"Function converts decimal fractions to binary fractions of bits lenth"""
+
+    # For negative numbers
+    if dec < 0:
+
+        binary = '1'
+        dec = dec*-1
+
+        for i in range(1, bits):
+            if (dec - (1 / (2 ** i)) >= 0):
+                dec = dec - 1 / (2 ** i)
+                binary += '0'
+
+            else:
+                binary += '1'
+
+    # For positive numbers
+    elif dec > 0:
+        binary = '0'
+
+        for i in range(1, bits):
+            if (dec - (1 / (2 ** i)) >= 0):
+                dec = dec - 1 / (2 ** i)
+                binary += '1'
+
+            else:
+                binary += '0'
+
+    # Dec is zero
+    else:
+        return '0'*bits
+
+    return binary
+
--- project/python/generator.py
+++ project/python/generator.py
@@ -1,5 +1,8 @@
 
 from math import exp
+from convert_func import *
+
+
 
 F = open('vhdl_code','w')
 
@@ -7,28 +10,26 @@ head = 's_x <= '
 F.write(head)
 
 
-bits = 16
+inbits = 8
+outbits = 8
 
-for i in range(0, int(2**bits)):
+# The negative side
+for i in range(0, int(2**(inbits-1))):
     n = bin(i)[2:]
+    n = '0'*(inbits-len(n)-1) + n
+    n = '1' + n
 
-    if  i == 0:
-        pre = ""
-    else:
-        pre = "               "
-    if len(n) < bits:
-        for j in range(0,bits - len(n)):
-            n = '0' + n
+    x = decToBinary(1/(1 + (exp(-binaryToDecimal(n)))),8)
 
-    x =  bin(int(1/(1 + exp(-i))*100))[2:]
-    if len(x) < bits:
-        for j in range(0,bits - len(x)):
-            x = '0' + x
-
-
-    txt = pre + '"' +  x + '"' +  " when x = " + '"'+ n + '"' + ' else' + '\n'
+    txt = '"' + str(x) + '"' + " when x = " + '"' + n + '"' + ' else' + '\n'
     F.write(txt)
 
+for i in range(1, int(2 ** (inbits - 1))):
+    n = bin(i)[2:]
+    n = '0' * (inbits - len(n) - 1) + n
+    n = '0' + n
 
+    x = decToBinary(1/(1 + (exp(-binaryToDecimal(n)))),8)
 
-
+    txt = '"' + str(x) + '"' + " when x = " + '"' + n + '"' + ' else' + '\n'
+    F.write(txt)
